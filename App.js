b9ff4c24e2dfc64ed8eaba18ee803f3980fd98e1
HAFDIAHMED <ahafdi62@gmail.com>
2021-09-26 22:11:39 +0100

    screens

--- App.js
+++ App.js
@@ -2,13 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-       <TextInput style={styles.input}
+      <View style={styles.inputcontainer}>
+      <TextInput style={styles.input}
        placeholder="email">
 
        </TextInput>
+       <TextInput style={styles.input}
+       placeholder="password">
+
+       </TextInput>
+      </View>
+      
     </View>
   );
 }
@@ -19,8 +27,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
+  },
+  inputcontainer : {
+    width : '80%',
   },
   input : {
-    backgroundColor:"green",
+    backgroundColor:"white",
+    paddingHorizontal: 15,
+    paddingVertical:10,
   },
 });
