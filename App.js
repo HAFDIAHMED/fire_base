7879c84a199dd99ae69b3a1c089a29e55d5607bb
HAFDIAHMED <ahafdi62@gmail.com>
2021-09-27 08:11:04 +0100

    changes

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import LoginSreen from './screens/LoginSreen';
 import HomeSreen from './screens/HomeSreen';
+
 export default function App() {
   return (
-  <HomeSreen />
+  <LoginSreen/>
   );
 }
