212a04cded75c7853d947c16b47b1b4abed66c08
HAFDIAHMED <ahafdi62@gmail.com>
2021-09-27 08:45:11 +0100

    stack navigator

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import LoginSreen from './screens/LoginSreen';
 import HomeSreen from './screens/HomeSreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
   return (
-  <LoginSreen/>
+  <LoginSreen/>   
   );
 }
